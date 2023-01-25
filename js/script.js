a53a1f890e1ee5bbb75449c816add5441f85c257
MartinaPerozzi <perozzimartina@gmail.com>
2023-01-25 15:06:22 +0100

    js - added comments

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
+// Chiamo l'elemento html che conterrà i box ("box-contain")e gli assegno una variabile
 const boxElement = document.querySelector(".box-contain");
 console.log(boxElement);
 
+// Creo un ciclo: fino che i = 100: 
+
 for (let i = 0; i = 3; i++) {
+
+    // Crea un elemento div;
 
     const boxItem = document.createElement("div");
     console.log(boxItem);
 
+    // Aggiungi all'elemento div la classe "box";
+
     boxItem.classList.add("box");
+
+    // Inserisci l'elemento "box" nella "boxElements";
     boxElement.append(boxItem);
 
 
