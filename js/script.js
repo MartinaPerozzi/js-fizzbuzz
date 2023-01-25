766b55ba15fc3b74d346cea34d62602c1bbefa40
MartinaPerozzi <perozzimartina@gmail.com>
2023-01-25 16:10:59 +0100

    html + css + js - exercise

--- js/script.js
+++ js/script.js
@@ -4,16 +4,16 @@ console.log(boxElement);
 
 // Creo un ciclo: fino che i = 100: 
 
-for (let i = 0; i = 3; i++) {
+for (let i = 1; i <= 100; i++) {
 
     // Crea un elemento div;
 
     const boxItem = document.createElement("div");
-    console.log(boxItem);
 
     // Aggiungi all'elemento div la classe "box";
 
     boxItem.classList.add("box");
+    console.log(boxItem);
 
     // Inserisci l'elemento "box" nella "boxElements";
     boxElement.append(boxItem);
@@ -21,11 +21,16 @@ for (let i = 0; i = 3; i++) {
     // Condizioni
     if (i % 3 == 0) {
         boxItem.classList.add("box-3");
+        boxItem.innerHTML += "Fizz";
     } else if (i % 5 == 0) {
         boxItem.classList.add("box-5");
+        boxItem.innerHTML += "Buzz";
     } else if ((i % 3 == 0) && (i % 5 == 0)) {
         boxItem.classList.add("box-5-3");
-
+        boxItem.innerHTML += "FizzBuzz";
+    } else {
+        boxItem.classList.add("box-empty");
+        boxItem.innerHTML += i;
     }
 }
 
